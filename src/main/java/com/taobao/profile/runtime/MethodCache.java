90379b0eeaae41bb03dcddf06e13b12597b5d4e1
caixiaojian <mc02cxj@gmail.com>
2017-12-24 18:40:20 +0800

    fix #4 Reduce the call to method fileWriter.append

--- src/main/java/com/taobao/profile/runtime/MethodCache.java
+++ src/main/java/com/taobao/profile/runtime/MethodCache.java
@@ -1,10 +1,9 @@
 /**
  * (C) 2011-2012 Alibaba Group Holding Limited.
- *
+ * <p>
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
- * 
  */
 package com.taobao.profile.runtime;
 
@@ -16,85 +15,82 @@ import com.taobao.profile.utils.DailyRollingFileWriter;
 
 /**
  * 方法名缓存,用ID代替方法名进行剖析,提升性能
- * 
+ *
  * @author luqi
  * @since 2010-6-23
  */
 public class MethodCache {
 
-	/**
-	 * 方法缓存默认大小
-	 */
-	private static final int INIT_CACHE_SIZE = 10240;
-	/**
-	 * 方法名缓存
-	 */
-	private static Vector<MethodInfo> mCacheMethods = new Vector<MethodInfo>(INIT_CACHE_SIZE);
-	
-	/**
-	 * 方法名writer
-	 */
-	private static DailyRollingFileWriter fileWriter = new DailyRollingFileWriter(Manager.METHOD_LOG_PATH);
+    /**
+     * 方法缓存默认大小
+     */
+    private static final int INIT_CACHE_SIZE = 10240;
+    /**
+     * 方法名缓存
+     */
+    private static Vector<MethodInfo> mCacheMethods = new Vector<MethodInfo>(INIT_CACHE_SIZE);
 
-	/**
-	 * 占位并生成方法ID
-	 * 
-	 * @return
-	 */
-	public synchronized static int Request() {
-		mCacheMethods.add(new MethodInfo());
-		return mCacheMethods.size() - 1;
-	}
+    /**
+     * 方法名writer
+     */
+    private static DailyRollingFileWriter fileWriter = new DailyRollingFileWriter(Manager.METHOD_LOG_PATH);
 
-	/**
-	 * 更新行号
-	 * 
-	 * @param id
-	 * @param linenum
-	 */
-	public synchronized static void UpdateLineNum(int id, int linenum) {
-		mCacheMethods.get(id).setMLineNum(linenum);
-	}
+    /**
+     * 占位并生成方法ID
+     *
+     * @return
+     */
+    public synchronized static int Request() {
+        mCacheMethods.add(new MethodInfo());
+        return mCacheMethods.size() - 1;
+    }
 
-	/**
-	 * 更新类名方法名
-	 * 
-	 * @param id
-	 * @param fileName
-	 * @param className
-	 * @param methodName
-	 */
-	public synchronized static void UpdateMethodName(int id, String fileName, String className, String methodName) {
-		MethodInfo methodInfo = mCacheMethods.get(id);
-		methodInfo.setMFileName(fileName);
-		methodInfo.setMClassName(className);
-		methodInfo.setMMethodName(methodName);
-	}
+    /**
+     * 更新行号
+     *
+     * @param id
+     * @param linenum
+     */
+    public synchronized static void UpdateLineNum(int id, int linenum) {
+        mCacheMethods.get(id).setMLineNum(linenum);
+    }
 
-	/**
-	 * 写出方法信息
-	 */
-	public synchronized static void flushMethodData() {
-		fileWriter.append("instrumentclass:");
-		fileWriter.append(Profiler.instrumentClassCount.toString());
-		fileWriter.append(" instrumentmethod:");
-		fileWriter.append(Profiler.instrumentMethodCount.toString());
-		fileWriter.append("\n");
+    /**
+     * 更新类名方法名
+     *
+     * @param id
+     * @param fileName
+     * @param className
+     * @param methodName
+     */
+    public synchronized static void UpdateMethodName(int id, String fileName, String className, String methodName) {
+        MethodInfo methodInfo = mCacheMethods.get(id);
+        methodInfo.setMFileName(fileName);
+        methodInfo.setMClassName(className);
+        methodInfo.setMMethodName(methodName);
+    }
 
-		Vector<MethodInfo> vector = mCacheMethods;
-		int size = vector.size();
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < size; i++) {
-			sb.append(i);
-			sb.append(' ');
-			sb.append(vector.get(i).toString());
-			sb.append('\n');
-			fileWriter.append(sb.toString());
-			sb.setLength(0);
-			if ((i % 50) == 0) {
-				fileWriter.flushAppend();
-			}
-		}
-		fileWriter.flushAppend();
-	}
+    /**
+     * 写出方法信息
+     */
+    public synchronized static void flushMethodData() {
+        String info = "instrumentclass:" + Profiler.instrumentClassCount + " instrumentmethod:" + Profiler.instrumentMethodCount + "\n";
+        fileWriter.append(info);
+
+        Vector<MethodInfo> vector = mCacheMethods;
+        int size = vector.size();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(i);
+            sb.append(' ');
+            sb.append(vector.get(i).toString());
+            sb.append('\n');
+            fileWriter.append(sb.toString());
+            sb.setLength(0);
+            if ((i % 50) == 0) {
+                fileWriter.flushAppend();
+            }
+        }
+        fileWriter.flushAppend();
+    }
 }
